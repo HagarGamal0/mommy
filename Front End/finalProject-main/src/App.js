03b0b7632d3c4ce01a50b4f589eb2340306f5ac3
Hagar <Hagar.Gamal@technokit.net>
2025-09-07 13:54:54 +0300

    message

--- Front End/finalProject-main/src/App.js
+++ Front End/finalProject-main/src/App.js
@@ -25,51 +25,107 @@ import CheckOut from "./Pages/CheckOut";
 import OrderDone from "./Components/ChecOut/OrderDone/OrderDone";
 import UserOrders from "./Components/userOrders/userOrders";
 
-
 function App() {
   const dispatch = useDispatch();
   const { _id } = useSelector((state) => state.ProductSlice.product);
-  const userId = localStorage.getItem('id');
- 
+  const userId = localStorage.getItem("id");
 
   useEffect(() => {
-    if(userId) {
+    if (userId) {
       dispatch(getUserData(userId));
-      dispatch(getCartItems(userId))
+      dispatch(getCartItems(userId));
     }
-
   }, [dispatch, userId]);
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<><Main /><Footer /></>} />
-        <Route path="home" element={<><Main /><Footer /></>} />
-        <Route path="products" element={<><Products /><Footer /></>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Main />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="home"
+          element={
+            <>
+              <Main />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="products"
+          element={
+            <>
+              <Products />
+              <Footer />
+            </>
+          }
+        />
         <Route path={_id} element={<ProductDetails />} />
         <Route path="about" element={<About />} />
-        <Route path="blog" element={<><Blog /><Footer /></>} />
+        <Route
+          path="blog"
+          element={
+            <>
+              <Blog />
+              <Footer />
+            </>
+          }
+        />
         <Route path="blogDetails" element={<BlogDetails />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
-        <Route path="profile" element={<><Profile /><Footer /></>}>
+        <Route
+          path="profile"
+          element={
+            <>
+              <Profile />
+              <Footer />
+            </>
+          }
+        >
           <Route path="info" element={<ProfileInfo />} />
           <Route path="edit" element={<EditInfo />} />
-          <Route path="products" element={<><SellerProducts /></>} />
-          <Route path='todo' element={<Todo />}/>
-          <Route path='babies' element={<Babies />}/>
-          <Route  path="myorders" element={<UserOrders/>}/>
+          <Route
+            path="products"
+            element={
+              <>
+                <SellerProducts />
+              </>
+            }
+          />
+          <Route path="todo" element={<Todo />} />
+          <Route path="babies" element={<Babies />} />
+          <Route path="myorders" element={<UserOrders />} />
         </Route>
-        <Route path="cart" element={<><ShoppingCart /><Footer /></>} /> 
-        <Route path="checkout" element={<><CheckOut /><Footer /></>} />
-        <Route path="orderDone" element={<OrderDone/>}/>
+        <Route
+          path="cart"
+          element={
+            <>
+              <ShoppingCart />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="checkout"
+          element={
+            <>
+              <CheckOut />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="orderDone" element={<OrderDone />} />
         {/* <Route path="*"  element={<Navigate to="404" replace={true}/>}/> */}
-      
       </Routes>
-
-      
-      
     </>
   );
 }
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# mommy
+# 👩‍👧 Mommy
+
 it is a website and mobile application that serves mothers, especially pregnant women, by knowing all the vaccinations or medical advice they need
+
+
+---
+
+## 🖼️ Screenshots
+![Main](docs/mommyy.PNG)
+![Services](docs/mommyy2.PNG)
+![WhoWeAre](docs/mommyy3.PNG)
+![Task Dashboard](docs/dashboard.jpg)
+
+---
+
+## 🔍 Features
+
+- 📝 Create, update, and delete baby stuf
+- 👩‍👧 Role-based access (e.g. Admin, Parent, Child)
+- 🔔 Notifications and reminders
+- 📊 Task status overview (e.g. completed, pending)
+
+---
+
+## ⚙️ Tech Stack
+
+- **Backend**: Node.js, Express.js
+- **Frontend**: React.js , Angular
+- **Database**: MongoDB 
+- **Authentication**: JWT 
+- **Others**: Bootstrap, Axios, etc.
+
+---
+
+## 🚀 Getting Started
+
+### Prerequisites
+
+- Node.js
+- npm or yarn
+- MongoDB or other DB
+
+### Installation
+
+```bash
+git clone https://github.com/HagarGamal0/mommy.git
+cd mommy
+npm install
